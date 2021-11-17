f89a21d5c0e75890f3176d685bf31bf997e2348a
AKrassilnikov <Stalker-artem87@mail.ru>
2021-11-17 12:55:41 +0600

    17.11.2021

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,22 +1,17 @@
 
 def can_make_pal(text):
-    d={}
+    dict_pal = {}
     for a in text:
-        if d.get(a) is None:
-            d[a]=1
-        else:
-            d[a]+=1
-    n=len(d)
-    e=0
-    for a in d.values():
-        if a%2==0:
-            e+=1
-    return e == n or e == n-1
+        dict_pal[a] = dict_pal.get(a,0) + 1
+    count =0
+    for a in dict_pal.values():
+        if a %2 != 0:
+            count += 1
+    return count <= 1
 
 text = input("Введите текст: ")
-result = can_make_pal(text)
-if result == 0:
-    print("Нельзя сделать палиндромом")
-else:
+if can_make_pal(text):
     print("Можно сделать палиндромом")
+else:
+    print("Нельзя сделать палиндромом")
 
