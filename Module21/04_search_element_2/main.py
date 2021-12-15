c4edbe60eb0f280f5f4236d47f2443c94774d233
AKrassilnikov <Stalker-artem87@mail.ru>
2021-12-15 11:52:12 +0600

    15.12.2021

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -10,18 +10,23 @@ site = {
         }
     }
 }
-def find_id(code_id,site):
+def find_id(code_id,site,dept):
     if code_id in site:
         return site[code_id]
-    for sub_site_code in site.values():
-        if isinstance(sub_site_code,dict):
-            sub_result = find_id(code_id,sub_site_code)  #рекурсия на поиск
-            if sub_result:
-                break
-    else:
-        sub_result = None
-    return sub_result
+    if dept > 1:
+        for sub_site_code in site.values():
+            if isinstance(sub_site_code,dict):
+                sub_result = find_id(code_id,sub_site_code,dept - 1)  #рекурсия на поиск
+                if sub_result:
+                    break
+        else:
+            sub_result = None
+        return sub_result
 
-code_id = input("Input id: ")
-result = find_id(code_id,site)
-print(result)
+code_id = input('Какой ключ ищем? ')
+dept = int(input('Введите глубину поиска: '))
+result = find_id(code_id,site,dept)
+if result:
+    print(result)
+else:
+    print('Такого ключа в структуре сайта нет.')
