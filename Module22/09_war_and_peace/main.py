830d79cbeb896d1ed1471d1da9ae1adf63cec257
AKrassilnikov <Stalker-artem87@mail.ru>
2021-12-28 15:28:10 +0600

    28.12.2021

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+
+import zipfile,os
+unzip = zipfile.ZipFile("voyna-i-mir.zip",'r')
+file = unzip.extract('voyna-i-mir.txt')
+voyna_i_mir = open('voyna-i-mir.txt','r',encoding='utf-8')
+summ_leters = 0
+for line in voyna_i_mir.readlines():
+    summ_leters += len(line)
+voyna_i_mir.close()
+print("Всего символов: ",summ_leters)
+
+
+
