13bf89577bfc346ae8dcbfacd3d646be09755699
KZ-ADM-Krassilnikov <Stalker>
2022-01-05 10:55:50 +0600

    05.01.2022

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -2,11 +2,8 @@
 import os
 data = input("Input data: ")
 input_path = input("Input dir path over space: ").split()
-file_path = r'C:\{}\{}\{}\{}\{}'.format(input_path[0],input_path[1],input_path[2],input_path[3],input_path[4])
-file_mane = input("Input file name: ")
-if os.path.exists(file_path):
-    open_file = open(os.path.join(file_path,file_mane),'w')
-    open_file.write(data)
-    open_file.close()
-else:
-    print("That path is not exist")
+file_name = input("Input file name: ")
+file_path = os.path.abspath(file_name)
+open_file = open(file_path,'w')
+open_file.write(data)
+open_file.close()
--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -14,15 +14,34 @@ text_file = open("text.txt",'w')
 text_file.write(input('Input text: '))
 text_file = open("text.txt",'r')
 count_leter,text_len = leter_count(text_file.read())
+
 leter_part = {}
 for key in count_leter.keys():
     leter_part[key] = round((count_leter[key] * 100/text_len)/100,3)
 leter_part_list= list(leter_part.items())
 leter_part_list.sort(key=lambda val: val[1],reverse=True)
-print(leter_part_list)
+
+new_sort_list = []
+sort_list = []
+for len in range(0,len(leter_part_list)):
+    try:
+        if leter_part_list[len][1] == leter_part_list[len + 1][1]:
+            new_sort_list.append(leter_part_list[len])
+        elif leter_part_list[len][1] != leter_part_list[len + 1][1]:
+            new_sort_list.append(leter_part_list[len])
+            new_sort_list.sort(key=lambda val: val[0])
+            sort_list.append(new_sort_list)
+            new_sort_list = []
+    except:
+        new_sort_list.append(leter_part_list[len])
+        new_sort_list.sort(key=lambda val: val[0])
+        sort_list.append(new_sort_list)
+        new_sort_list = []
+
 text_file = open("analysis.txt", 'w')
-for pare in leter_part_list:
-    text_file.writelines([pare[0],':',str(pare[1]),'\n'])
+for pare in (sort_list):
+    for sub_pare in pare:
+        text_file.writelines([sub_pare[0],':',str(sub_pare[1]),'\n'])
 text_file.close()
 
 
