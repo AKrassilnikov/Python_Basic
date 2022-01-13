bfcb778481dd43abfe0a39284763e8d2033b96b3
AKrassilnikov <Stalker-artem87@mail.ru>
2022-01-13 10:29:33 +0600

    13.01.2021

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -1,39 +1,31 @@
 import random
 
-
 def f(x, y):
     x += random.randint(0, 10)
     y += random.randint(0, 5)
-    return x / y
-
+    try:
+        return round(y / x, 2)
+    except ZeroDivisionError:
+        return 0
 
 def f2(x, y):
     x -= random.randint(0, 10)
     y -= random.randint(0, 5)
-    return y / x
+    try:
+        return round(y / x, 2)
+    except ZeroDivisionError:
+        return 0
 
-
-try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
+with open('coordinates.txt', 'r') as file:
+    for line in file.readlines():
         nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
-            res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
+        res1 = f(float(nums_list[0]), float(nums_list[1]))
+        res2 = f2(float(nums_list[0]), float(nums_list[1]))
+        number = random.randint(0, 100)
+        file_2 = open('result.txt', 'w')
+        my_list = sorted([str(res1), str(res2), str(number)])
+        file_2.write(' '.join(my_list))
+        file_2.close()
 
 
-# TODO отредактировать и исправить программу
+
