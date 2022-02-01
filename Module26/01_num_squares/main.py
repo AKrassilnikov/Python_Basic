18d18bb3cfd7e8c16554b10befa990641d7fa122
SHAMAN <SHAMAN@mail.ru>
2022-02-01 16:27:18 +0600

    01.02.2021

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+class Square:
+    def __init__(self,number: int) -> None:
+        self.number = number
+        self.count = 0
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.count += 1
+        if self.count > self.number:
+            raise StopIteration
+        return self.count ** 2
+
+
+def gen_sqr(number):
+    for num in range(1,number + 1):
+        yield num ** 2
+
+number = int(input("Enter number: "))
+class_sqr = Square(number)
+
+print("Class generator:")
+for num in class_sqr:
+    print(num, end=" ")
+
+print("\nFunction generator: ")
+for num in gen_sqr(number):
+    print(num, end=" ")
+
+print("\nGen expression: ")
+
+
+numbers = (num ** 2 for num in range(1,number + 1))
+print(*numbers)
+
