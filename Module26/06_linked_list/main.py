8a7c57124bd1bb1aee0b73842973caca6e089978
KZ-ADM-Krassilnikov <Stalker>
2022-02-03 14:27:00 +0600

    03.02.2022

--- Module26/06_linked_list/main.py
+++ Module26/06_linked_list/main.py
@@ -1,60 +1,65 @@
+from typing import Any,Optional
+
+class Node:
+    def __init__(self,value: Optional[Any] = None, next : Optional['None'] = None ) -> None:
+        self.value = value
+        self.next = next
+
+    def __str__(self) -> str:
+        return "Node [{value}]".format(value=str(self.value))
+
 class LinkedList:
-    class __Node:
-        def __init__(self, value):
-            self.value = value
-            self.nxt = None
+    def __init__(self) -> None:
+        self.head = None
+        self.length = 0
 
-    def __init__(self, *args):
-        length = len(args)
-        self.__length = length
-        self.__head = self.__Node(args[0]) if length > 0 else None
-        self.__tail = self.__head
-        for i in range(1, length):
-            self.__tail.nxt = self.__Node(args[i])
-            self.__tail = self.__tail.nxt
+    def __str__(self) -> str:
+        if self.head is not None:
+            current = self.head
+            value = [str(current.value)]
+            while current.next is not None:
+                current = current.next
+                value.append(str(current.value))
+            return "[{value}]".format(value = " ".join(value))
+        return  "LinkedList []"
 
-    def __iter__(self):
-        current = self.__head
-        while current is not None:
-            yield current.value
-            current = current.nxt
+    def append(self, elem: Any) -> None:
+        next_node = Node(elem)
+        if self.head is None:
+            self.head = next_node
+            return
 
-    def __str__(self):
-        return f"[{' '.join(str(i) for i in self)}]"
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = next_node
+        self.length += 1
 
-    def __len__(self):
-        return self.__length
-
-    def __index_check(self, index):
-        if not 0 <= index < self.__length:
+    def remove(self,index) -> None:
+        cur_node = self.head
+        cur_index = 0
+        if self.length == 0 or self.length <= index:
             raise IndexError
+        if cur_node is not None:
+            if index == 0:
+                self.head = cur_node.next
+                self.length -= 1
+                return
+        while cur_node is not None:
+            if cur_index == index:
+                break
+            prev = cur_node
+            cur_node = cur_node.next
+            cur_index += 1
+        prev.next = cur_node.next
+        self.length -= 1
 
-    def append(self, value):
-        if self.__length > 0:
-            self.__tail.nxt = self.__Node(value)
-            self.__tail = self.__tail.nxt
-        else:
-            self.__head = self.__tail = self.__Node(value)
-        self.__length += 1
 
-    def get(self, index):
-        self.__index_check(index)
-        current = self.__head
-        for _ in range(index):
-            current = current.nxt
-        return current.value
-
-    def remove(self, index):
-        self.__index_check(index)
-        if self.__length == 1:
-            self.__head = self.__tail = None
-        elif index == 0:
-            self.__head = self.__head.nxt
-        else:
-            current = self.__head
-            for _ in range(index - 1):
-                current = current.nxt
-            current.nxt = current.nxt.nxt
-            if index == self.__length - 1:
-                self.__tail = current
-        self.__length -= 1
+my_list = LinkedList()
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+print(my_list)
+print()
+my_list.remove(0)
+print(my_list)
