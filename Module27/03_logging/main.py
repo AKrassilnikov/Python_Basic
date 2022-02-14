7ce3edd4d700e362e7e82fb05ef34ccc67b0f3ca
AKrassilnikov <Stalker-artem87@mail.ru>
2022-02-14 14:17:58 +0600

    14.02.2022 - 2

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from typing import Callable,Any
+from datetime import datetime
+
+
+def logging(func:Callable) -> Callable:
+    """ Функция с запросом и с логированием ошибки в файл """
+
+    print(f'Function name: {logging.__name__}, Description: {logging.__doc__}\n')
+
+    print("Как дела?",end=" ")
+    answer = input()
+    if not isinstance(answer,str):
+        with open("function_errors.log",'a') as log_file:
+            error_log = f"{SyntaxError} : {datetime.now()}"
+            log_file.write(error_log)
+    print("А у меня не очень! Ладно, держи свою функцию.")
+    return func
+
+
+@logging
+def test() -> Any:
+    print('<Тут что-то происходит...>')
+
+test()
