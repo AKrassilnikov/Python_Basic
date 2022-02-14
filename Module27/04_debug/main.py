fe2417a118f6c1bad3f9c1dcae4dd72925bfdb24
AKrassilnikov <Stalker-artem87@mail.ru>
2022-02-14 15:32:46 +0600

    14.02.2022

--- Module27/04_debug/main.py
+++ Module27/04_debug/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+
+def debug(name):
+    def wrapper(*args,**kwargs):
+        print(f"Вызывыется {name.__name__},{args},{kwargs}")
+        result = name(*args,**kwargs)
+        print(result,"\n")
+    return wrapper
+
+@debug
+def greeting(name, age=None) -> str:
+    """ That function returns person info """
+
+    if age:
+        return "Ого, {name}! Тебе уже {age} лет, ты быстро растёшь!".format(name=name, age=age)
+    else:
+        return "Привет, {name}!".format(name=name)
+
+if __name__ == '__main__':
+    greeting("Том")
+    greeting("Миша", age=100)
+    greeting(name="Катя", age=16)
