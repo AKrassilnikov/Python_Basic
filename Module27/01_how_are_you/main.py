d9eef46003d0f4e753277f02a642f6290f021778
AKrassilnikov <Stalker-artem87@mail.ru>
2022-02-16 16:17:56 +0600

    16.02.2022

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,20 +1,32 @@
 from typing import Callable,Any
 
-def how_are_you(func:Callable) -> Callable:
-    """ Функция с запросом """
+def how_are_you(func: Callable) -> Callable:
+    """
+    Декоратор для взаимодействия с пользователем
 
-    print("Как дела?",end=" ")
-    input()
-    print("А у меня не очень! Ладно, держи свою функцию.")
-    return func
-
+    :param func:
+    :return:
+    """
+    # @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        """
+        Функция - обертка
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
 
 @how_are_you
-def test() -> Any:
+def test():
     print('<Тут что-то происходит...>')
 
-if __name__== '__main__':
-    test()
+test()
+
 
 
 
