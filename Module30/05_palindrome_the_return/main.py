8ddbaa291124cf3423ea8b00a83e65083ecf7f28
AKrassilnikov <Stalker-artem87@mail.ru>
2022-03-10 16:27:24 +0600

    10.03.2022

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,4 +1,6 @@
 
+from collections import Counter
+
 def can_be_poly(string):
     dict_pal = {}
     for a in string:
@@ -9,5 +11,12 @@ def can_be_poly(string):
             count += 1
     return count <= 1
 
+
+def can_be_poly2(string):
+    return len(list(filter(lambda x: x % 2 ,Counter(string).values()))) <= 2
+
 print(can_be_poly('ababc'))
 print(can_be_poly('abbbc'))
+
+print(can_be_poly2('ababc'))
+print(can_be_poly2('abbbc'))
