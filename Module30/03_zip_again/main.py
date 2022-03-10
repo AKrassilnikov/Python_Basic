cff6f4077ca68b265f395978faeede6ef47adddb
AKrassilnikov <Stalker-artem87@mail.ru>
2022-03-10 15:48:51 +0600

    10.03.2022

--- Module30/03_zip_again/main.py
+++ Module30/03_zip_again/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+from typing import List
+
+strings: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+new_list = list(map(lambda leter, number : (leter,number), strings,numbers))
+print(new_list)
--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+print("Мой вариант")
+number_list = (num for num in range(1000))
+count = 0
+for num in number_list:
+    count += 1
+    print(num,end = "\t")
+    if count == 20:
+        count = 0
+        print()
+
+print()
+print("Односточный")
+number_list_2 = [num for num in range(1000)]
+print(number_list_2)
