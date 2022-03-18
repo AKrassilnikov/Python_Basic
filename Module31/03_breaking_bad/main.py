6ab6fcd9c37e862829f349f3c77527bd288a0eb1
AKrassilnikov <Stalker-artem87@mail.ru>
2022-03-18 16:43:18 +0600

    18.03.2022

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -4,7 +4,10 @@ result = requests.get('https://www.breakingbadapi.com/api/deaths')
 text = json.loads(result.text)
 with open('episode.json','w') as file:
     json.dump(text[63],file,indent=4)
+with open('episode.json','r') as j_file:
+    for i in j_file.readlines():
+        print(i,end='')
 
-
+#
 
 
